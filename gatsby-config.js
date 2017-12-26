39ae0460feba7b71e455d15ee17a68d84015bc29
mchernin34 <mchernin21@gmail.com>
2017-12-26 17:42:56 +0200

    Added WP source plugin credentials

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        baseUrl: 'https://mchernin34.wordpress.com/',
+        protocol: 'https',
+        hostingWPCOM: true,
+        useACF: false,
+        auth: {
+          wpcom_app_clientSecret: 'CLIENT SECRET',
+          wpcom_app_clientId: 'CLIENT ID',
+          wpcom_user: 'USERNAME',
+          wpcom_pass: 'PASSWORD'
+        },
+        verboseOutput: false
+      }
+    }
+  ]
 }
